ff1cb8dd74cbd8def5ad061ac9dc52067bf4380e
adamsade <aderajewgebre11@gmail.com>
2023-03-16 18:56:10 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * return: 0
+ */
+int main(void)
+{
+	pritf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0); /* using return in main ends the program */
+}
