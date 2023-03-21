27acd0d809a5e7edfbb277cbb0340cdc720b43b2
adamsade <aderajewgebre11@gmail.com>
2023-03-21 15:48:19 -0700

    funcions and nl

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,13 +3,13 @@
 /**
  * print_alphabet - function that prints alphabet
  *
- * Return: always 0;
+ * Return: always 0
  */
 void print_alphabet(void)
 {
-	char i;
-	
-	for (i = 'a'; i <= 'z';  i++)
-		_putchar(i);
-	_putchar("\n");
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
 }
