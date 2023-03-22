e81fa1f2581d12e64e2f29cc02155019774aa397
adamsade <aderajewgebre11@gmail.com>
2023-03-22 13:01:18 -0700

    5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_sign - function that prints signs 
+ * print_sign - function that prints signs
  * @n: function parameter
  *
  * Return: 1, 0 and -1
@@ -10,23 +10,23 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('43');
-		putchar(',');
-		putchar(' ');
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		putchar('48');
-		putchar(',');
-		putchar(' ');
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
 		return (0);
 	}
 	else
 	{
-		putchar('45');
-		putchar(',');
-		putchar(' ');
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
 		return (-1);
 	}
 }
