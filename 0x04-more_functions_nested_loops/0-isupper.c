783b034fe9c8cb433111a996651c464c34d6a94b
adamsade <aderajewgebre11@gmail.com>
2023-03-24 00:46:23 -0700

    1-isdigit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper _ print uppercase letters
+ * @c: the characters to be checked
+ *
+ * Return: 1 for all uppercases or 0 for anything else
+ */
+int isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
