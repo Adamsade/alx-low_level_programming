b2dc78c18341c45849be64b94c5fd6421c45200f
adamsade <aderajewgebre11@gmail.com>
2023-04-03 07:42:21 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int _putchar(char c);
+void print_diagsums(int *a, int size);
+void print_chessboard(char (*a)[8]);
+char *_strstr(char *haystack, char *needle);
+char *_strpbrk(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept);
+char *_memset(char *s, char b, unsigned int n);
+char *_strstr(char *haystack, char *needle);
